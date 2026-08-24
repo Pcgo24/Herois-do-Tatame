86131f643f51c9efd5e4be913246647008d6b064
Paulo Cesar Cardoso Domingues <paulocesarcrds4@gmail.com>
2026-08-24 20:54:23 -0300

    feat: cpf em users e seeder do professor

---
+++ config/professor.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'cpf' => env('PROFESSOR_CPF', '12345678909'),
+    'password' => env('PROFESSOR_PASSWORD', 'heroisdotatame'),
+    'name' => env('PROFESSOR_NAME', 'Professor'),
+    'email' => env('PROFESSOR_EMAIL', 'professor@heroisdotatame.local'),
+];
